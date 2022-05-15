312ae0f234e2a0a2f9cf1084a91dee2b2525d9ee
jorge moreno <morenojorge308@gmail.com>
2022-05-15 17:21:47 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import PayPal from './Components/Paypal';
 
 function App() {
+  const [checkout,setCheckout]=useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {
+
+      checkout?(<PayPal/>):
+      (
+        <button onClick={()=>{setCheckout(true);}}>Checkout</button>
+      )  
+
+      }
+  </div>
+   
   );
+
 }
 
 export default App;
